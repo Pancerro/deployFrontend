ec7a1769319b314f11d7e7f055f778ac511d6c2a
Pancerro <pancerro@gmail.com>
2021-01-21 08:15:21 +0100

    deploy

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
+const cors = require('cors');
 const app = express();
+const whitelist = ['https://ue-form-backend-app.herokuapp.com/']; // list of allow domain
 
+const corsOptions = {
+    origin: function(origin, callback) {
+        if (!origin) {
+            return callback(null, true);
+        }
+
+        if (whitelist.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}
+
+// end 
+app.use(cors(corsOptions));
 // Serve only the static files form the angularapp directory
 app.use(express.static(__dirname + '/angularapp'));
 
@@ -14,3 +32,5 @@ app.get('/*', function(req, res) {
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+// add this cod
