82ea4adc468fafc3c804ac554c4785a7ba4ae525
Pancerro <pancerro@gmail.com>
2021-01-21 08:26:44 +0100

    deploy

--- server.js
+++ server.js
@@ -1,27 +1,7 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
 const app = express();
-const whitelist = ['https://ue-form-backend-app.herokuapp.com/']; // list of allow domain
-
-const corsOptions = {
-    origin: function(origin, callback) {
-        if (!origin) {
-            return callback(null, true);
-        }
-
-        if (whitelist.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}
-
-// end 
-app.use(cors(corsOptions));
 // Serve only the static files form the angularapp directory
 app.use(express.static(__dirname + '/angularapp'));
 
