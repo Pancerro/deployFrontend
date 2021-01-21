20e7328c5cccda48d8b2b9c242763b568e33a813
Pancerro <pancerro@gmail.com>
2021-01-21 06:26:29 +0100

    deploy v1

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8081);
+// Serve only the static files form the angularapp directory
+app.use(express.static(__dirname + '/angularapp'));
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/myWebsite/index.html'));
+
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
