0d6136cf14efc72a2bed22ff78fdbbd947f665f5
Pancerro <pancerro@gmail.com>
2021-06-01 00:49:39 +0200

    dockerfile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpInterceptorService} from './admin/login/login-service/http-interceptor/http-interceptor.service';
 import { AdminModule } from './admin/admin.module';
 import { MemberModule } from './user/member.module';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-    ],
+  declarations: [
+    AppComponent,
+  ],
 
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        FormsModule,
-        HttpClientModule,
-        AdminModule,
-        MemberModule,
-    ],
-    providers: [],
-    exports: [
-    ],
-    bootstrap: [AppComponent]
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    AdminModule,
+    MemberModule,
+  ],
+  providers: [],
+  exports: [
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/user/member.module.ts
+++ src/app/user/member.module.ts
@@ -3,18 +3,19 @@ import { CommonModule } from '@angular/common';
 import { FormComponent } from './form/form.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ModalModule } from '../dialog/modal.module';
+import {RouterModule} from '@angular/router';
 
 
 
 @NgModule({
-    declarations: [FormComponent],
-    imports: [
-        CommonModule,
-        HttpClientModule,
-        ModalModule,
-    ],
-    providers: [],
-    exports: [
-    ]
+  declarations: [FormComponent],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    ModalModule,
+  ],
+  providers: [],
+  exports: [
+  ]
 })
 export class MemberModule { }
